243de8517d772e7343f2d8b7a9a881ee72200b6e
Stephen Johnson <smj8jqu@virginia.edu>
2023-05-03 12:46:15 -0400

    Web stuff up and running through radii w/ connections.

--- web/Server.py
+++ web/Server.py
@@ -1,8 +1,43 @@
 from flask import Flask, request
-
+import json
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
 app = Flask(__name__)
 
-@app.route("/webhook", methods=['POST'])
-def receiver():
-    print("Data: "+request.json)
-    return "Received"
+app.config['MQTT_BROKER_URL'] = 'nam1.cloud.thethings.network'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'group11app@ttn'
+app.config['MQTT_PASSWORD'] = 'NNSXS.IYW7VFU4FDUSCDDL4A5HHCGPMEY6N22B6Z44SGI.SUJKEKTTIPXNM45WVYMDCTOMX7PNJPLJCKEX2YC5WPTCNRFPBD4A'
+app.config['MQTT_TLS_ENABLED'] = False
+topic = 'v3/+/devices/+/up'
+
+mqtt_client = Mqtt(app)
+socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    if(rc == 0):
+        print("connected")
+        mqtt_client.subscribe(topic)
+    else:
+        print('Not connected')
+
+@socketio.on('connect')
+def connect_socket():
+    print('client connected')
+    socketio.emit('hello','Connected to Flask websocket')
+
+@mqtt_client.on_message()
+def messages(client, userdata, message):
+    print("message recieved: "+ message.payload)
+    lat = float(message.payload[0:7])
+    lng = float(message.payload[8:15])
+    speed = float(message.payload[16:])
+    outgoing = {"lat":lat, "lng":lng, "speed":speed}
+    socketio.emit('ttn_data', outgoing)
+
--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {io} from 'socket.io-client'
+import React, {useState, useEffect, useMemo} from 'react';
+import {GoogleMap, MarkerF, LoadScript, CircleF} from '@react-google-maps/api'
+let socket;
+
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [markers, setMarkers] = useState([{ lat: 38.032750, lng:  -78.510423, speed: 5.0, battery: 100}]);
+
+
+  const center = useMemo(() => ({ lat: 38.032750, lng:  -78.510423 }), []);
+
+  useEffect(() => {
+    
+    socket = io('http://localhost:5000',{forceNew:true});
+
+    socket.on('hello', (new_data) => {
+      console.log(new_data)
+      setData(data => [...data, new_data])
+    })
+
+    socket.on("ttn_data",(new_marker) => {
+      console.log(new_marker)
+      setMarkers(markers => [...markers, new_marker])
+
+    })
+
+    return (() => {
+      socket.disconnect();
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
+        <GoogleMap
+            mapContainerClassName='map-container'
+            center={center}
+            zoom={18.5}
+          >
+            <MarkerF 
+                    position={{lat: 38.031770, lng: -78.511003}}
+                    label={{text: "Home Base", fontWeight: "bolder"}}
+                    key={-1}
+                    >
+
+            </MarkerF>
+            {
+              markers.map((marker, i) => {
+                return(
+                  <div>
+                    <MarkerF 
+                      position={{lat:marker.lat, lng:marker.lng}}
+                      label={{text: marker.speed+"m/s", fontWeight: "bolder"}}
+                      key={i}
+                      >
+
+                    </MarkerF>
+                    <CircleF
+                      visible={(i == markers.length-1)}
+                      radius={2*marker.battery}
+                      fillColor={"#ff0000"}
+                      fillOpacity={0.35}
+                      center={{lat:marker.lat, lng:marker.lng}}
+                    ></CircleF>
+                  </div>
+                )
+              })
+            }
+        </GoogleMap>
+      </LoadScript>
+        
     </div>
   );
 }
